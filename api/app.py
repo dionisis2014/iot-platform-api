443e7161abd9e5ed5490cb9016a9a613cf328fa4
dionisis2014 <toulatosdionisis@gmail.com>
2023-01-03 22:47:29 +0200

    Generate more random data for testing

--- api/app.py
+++ api/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+import random
 
 app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/api")
+@cross_origin()
 def status():
 	msg = {}
 	msg['status'] = "OK"
@@ -13,6 +19,7 @@ def status():
 	return msg
 
 @app.route("/api/<user_id>")
+@cross_origin()
 def user(user_id):
 	msg = {}
 	msg['id'] = user_id
@@ -22,91 +29,22 @@ def user(user_id):
 		'online': 13,
 		'offline': 2,
 	}
-	msg['sensors'] = [
-		{
-			'id': '87se7fs',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': 'My Sensor',
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': 'My Sensor 2',
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': 'My Sensor 3',
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': False
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': 'My Sensor 4',
-			'online': False
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		},
-		{
-			'id': '8f7sd89',
-			'name': None,
-			'online': True
-		}
-	]
+	msg['sensors'] = []
+	for i in range(1, random.randrange(10, 30)):
+	    msg['sensors'].append({
+	        'id': i,
+	        'name': 'Name',
+	        'online': True
+	    })
 	return msg
 
 @app.route("/api/<user_id>/<sensor_id>")
+@cross_origin()
 def sensor(user_id, sensor_id):
 	msg = {}
 	msg['id'] = sensor_id
 	msg['name'] = 'Name'
-	msg['type'] = 'Type'
-	msg['online'] = True
+	msg['type'] = random.choice(['temp', 'humid', 'light', 'winds', 'windd', 'idk'])
+	msg['online'] = bool(random.getrandbits(1))
 	msg['value'] = 123.456
 	return msg
